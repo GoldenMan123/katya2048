10e053a8c0bc9b7cfefb87e8b265d0ddc9000272
Vladimir Zhukov <zhvv117@gmail.com>
2015-02-27 22:03:41 +0300

    Destroyed

--- 2048_files/html_actuator.js
+++ 2048_files/html_actuator.js
@@ -13,26 +13,7 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 
   window.requestAnimationFrame(function () {
     self.clearContainer(self.tileContainer);
-
-    grid.cells.forEach(function (column) {
-      column.forEach(function (cell) {
-        if (cell) {
-          self.addTile(cell);
-        }
-      });
-    });
-
-    self.updateScore(metadata.score);
-    self.updateBestScore(metadata.bestScore);
-
-    if (metadata.terminated) {
-      if (metadata.over) {
-        self.message(false); // You lose
-      } else if (metadata.won) {
-        self.message(true); // You win!
-      }
-    }
-
+    self.message(false);
   });
 };
 
@@ -132,7 +113,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "Я всё сломал :(";
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
